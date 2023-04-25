243d2757a43e0106c97a07a83b459b8c50142bfc
DELL <DELL@Akarapuswamy>
2023-04-25 11:34:06 +0530

    HibernateInheritanceJion

---
+++ hibernateinheritancejion/src/main/java/util/Player.java
@@ -0,0 +1,61 @@
+package util;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Player {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String name;
+	@Column
+	private String bodytype;
+	
+	public Player( String name, String bodytype) {
+		this.name = name;
+		this.bodytype = bodytype;
+	}
+
+	public Player() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBodytype() {
+		return bodytype;
+	}
+
+	public void setBodytype(String bodytype) {
+		this.bodytype = bodytype;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", name=" + name + ", bodytype=" + bodytype + "]";
+	}
+}
